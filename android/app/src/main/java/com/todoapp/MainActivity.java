36d6c5d39d11ab133150447d6900cccb24f28677
Sebastian Perez <sperezm97@gmail.com>
2019-02-07 15:59:21 -0400

    navigation done

--- android/app/src/main/java/com/todoapp/MainActivity.java
+++ android/app/src/main/java/com/todoapp/MainActivity.java
@@ -1,15 +1,16 @@
 package com.todoapp;
 
 import com.facebook.react.ReactActivity;
+import com.reactnativenavigation.NavigationActivity;
 
-public class MainActivity extends ReactActivity {
+public class MainActivity extends NavigationActivity {
 
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
      */
-    @Override
-    protected String getMainComponentName() {
-        return "TodoApp";
-    }
+    // @Override
+    // protected String getMainComponentName() {
+    //     return "TodoApp";
+    // }
 }

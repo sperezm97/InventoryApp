2992325943b6d31bbcaba20c40d491fca61bab54
Sebastian Perez <sperezm97@gmail.com>
2019-03-03 11:53:41 -0400

    axios config

---
+++ src/config/axios.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+// Set config defaults when creating the instance
+const instance = axios.create({
+  baseURL: 'https://api.example.com',
+  headers: {
+
+  }
+});
+
+
+// Add a request interceptor
+instance.interceptors.request.use(
+  (config) => {
+    // Do something before request is sent
+    return config;
+  },
+  (error) => {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor
+instance.interceptors.response.use(
+  (response) => {
+    // Do something with response data
+    return response;
+  },
+  (error) => {
+    // Do something with response error
+    return Promise.reject(error);
+  }
+);
+
+export default instance;

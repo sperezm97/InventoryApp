fa925062847a5005fc273c0aa0fa4d8dda17c28a
Sebastian Perez <sperezm97@gmail.com>
2019-03-06 09:49:36 -0400

    Add Screen list Item

--- src/containers/registrationScreen.js
+++ src/containers/registrationScreen.js
@@ -3,6 +3,7 @@ import Login from '@containers/Login';
 import Drawer from '@containers/Drawer';
 import Dashboard from '@containers/Dashboard';
 import Register from '@containers/Register';
+import ListArticles from '@containers/ListArticles';
 
 import WrapperProvider from '@config/WrapperProvider';
 import * as screen from '@containers/screens';
@@ -13,4 +14,6 @@ export default function registrationScreen() {
   Navigation.registerComponent(screen.SCREEN_REGISTER, () => WrapperProvider(Register));
   Navigation.registerComponent(screen.SCREEN_DASHBOARD, () => WrapperProvider(Dashboard));
   Navigation.registerComponent(screen.SCREEN_DRAWER, () => WrapperProvider(Drawer));
-}
+  Navigation.registerComponent(screen.SCREEN_LIST_ARTICLES, () => WrapperProvider(ListArticles));
+
+};
--- src/containers/screens.js
+++ src/containers/screens.js
@@ -2,3 +2,4 @@ export const SCREEN_LOGIN = 'inv.Login';
 export const SCREEN_REGISTER = 'inv.Register';
 export const SCREEN_DASHBOARD = 'inv.Dashboard';
 export const SCREEN_DRAWER = 'inv.Drawer';
+export const SCREEN_LIST_ARTICLES = 'inv.listArticles';

690b2242f9266559b4d34e7d92a006cfcd78828f
Sebastian Perez <sperezm97@gmail.com>
2019-03-06 15:48:27 -0400

    done login and Registrarion screen

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,19 +1,68 @@
-import React, { Component } from 'react'
-import { View, Text } from 'react-native'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
+import React, { Component } from 'react';
+import { Image } from 'react-native';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Container, Content, Item, Input, Button, Icon, View, Text } from 'native-base';
+import _ from 'lodash';
 
 class Login extends Component {
   static propTypes = {
 
   }
 
+  static navigatorStyle = {
+
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {
+        email: '',
+        password: ''
+      },
+      error: '',
+      isLoading: null
+    }
+  }
+
+
+  setUserInfo(property: string, value: string) {
+    const data = _.cloneDeep(this.state.data);
+    data[property] = value;
+    this.setState({ data });
+  }
+
   render() {
     return (
-      <View>
-        <Text> prop </Text>
-      </View>
-    )
+      <Container>
+        <Content padder>
+          <View>
+            {/* <Image source={{ uri: '' }} /> */}
+          </View>
+          <View>
+            <Item rounded>
+              <Icon name="ios-contact" type="Ionicons" />
+              <Input placeholder="mail@mail.com" />
+            </Item>
+            <Item rounded last>
+              <Icon name="ios-key" type="Ionicons" />
+              <Input placeholder="*******" />
+            </Item>
+            <View>
+              <Button iconLeft success>
+                <Icon name="login-variant" type="MaterialCommunityIcons" />
+                <Text>Sign in</Text>
+              </Button>
+              <Button iconLeft bordered success>
+                <Icon name="user-follow" type="SimpleLineIcons" />
+                <Text>Register</Text>
+              </Button>
+            </View>
+          </View>
+        </Content>
+      </Container>
+    );
   }
 }
 
@@ -25,5 +74,4 @@ const mapDispatchToProps = {
 
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Login)
-
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
--- src/containers/Register/index.js
+++ src/containers/Register/index.js
@@ -1,19 +1,86 @@
-import React, { Component } from 'react'
-import { View, Text } from 'react-native'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
+import React, { Component } from 'react';
+import { } from 'react-native';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Container, Content, Title, Button, Icon, View, Text } from 'native-base';
+import TitleBar from '@components/TitleBar';
+import { globalStyle } from '@config/globalStyles';
+import InputStyle from '@components/InputStyle';
 
 class Register extends Component {
   static propTypes = {
 
   }
 
-  render() {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: {
+        email: '',
+        password: '',
+        confirmPassword: '',
+
+      },
+      error: null,
+      isloading: null
+    };
+  }
+
+  renderLeftTitleBar() {
     return (
-      <View>
-        <Text> prop </Text>
-      </View>
-    )
+      <Button transparent small icon>
+        <Icon active name="ios-arrow-back" type="Ionicons" style={globalStyle.iconSize} />
+      </Button>
+    );
+  }
+
+  renderCenterTitleBar() {
+    return (
+      <Title>Register</Title>
+    );
+  }
+
+  render() {
+    const { email, password, confirmPassword } = this.state.data;
+    return (
+      <Container>
+        <TitleBar
+          left={this.renderLeftTitleBar()}
+          body={this.renderCenterTitleBar()}
+          right={<View />}
+        />
+        <Content style={globalStyle.globalContainer}>
+          <View style={globalStyle.contentContainer}>
+            <InputStyle
+              onChangeText={value => this.setState(prevState => ({ data: { ...prevState.data, email: value } }))}
+              value={email}
+              placeholder="Email"
+            />
+            <InputStyle
+              onChangeText={value => this.setState(prevState => ({ data: { ...prevState.data, password: value } }))}
+              value={password}
+              placeholder="Password"
+              password
+            />
+            <InputStyle
+              onChangeText={value => this.setState(prevState => ({ data: { ...prevState.data, confirmPassword: value } }))}
+              value={confirmPassword}
+              placeholder="Confirm Password"
+              password
+            />
+          </View>
+          <View>
+            <Button block success large>
+              <Text>
+                Save
+              </Text>
+            </Button>
+          </View>
+
+        </Content>
+      </Container>
+    );
   }
 }
 
@@ -25,5 +92,4 @@ const mapDispatchToProps = {
 
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Register)
-
+export default connect(mapStateToProps, mapDispatchToProps)(Register);
